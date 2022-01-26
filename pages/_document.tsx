0fcf681ef4dc01acf71d03adfaf0dfd0e55f8f50
Quynh Nguyen <nguyenp_quynh@kiaisoft.com>
2022-01-26 10:43:21 +0700

    Fix font notosan JP

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,7 +16,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap"
             rel="stylesheet"
           />
           {/* Inject MUI styles first to match with the prepend: true configuration. */}
