5cc28657c013a18a713f14007fd6a31bb0836800
Quynh Nguyen <nguyenp_quynh@kiaisoft.com>
2022-04-01 08:21:35 +0700

    Fix login twitter and login

--- src/components/home/IndexComponent.tsx
+++ src/components/home/IndexComponent.tsx
@@ -1,7 +1,7 @@
 import { Box } from "@mui/material";
 import { useTranslation } from "next-i18next";
 import React, { useEffect, useState } from "react";
-import { useQuery } from "react-query";
+// import { useQuery } from "react-query";
 
 import ContentComponent from "src/components/layouts/ContentComponent";
 import {
@@ -12,7 +12,7 @@ import {
   sendMatchingRequest,
 } from "src/services/user";
 
-import { REACT_QUERY_KEYS } from "../constants/constants";
+// import { REACT_QUERY_KEYS } from "../constants/constants";
 
 import BannerComponent from "./blocks/BannerComponent";
 import MatchingComponent from "./blocks/MatchingComponent";
@@ -28,50 +28,61 @@ const HomeIndexComponents = () => {
   const [memberRecommends, setMemberRecommends] = useState([]);
 
   // get users_provinces
-  const { data: userProvinceData } = useQuery(REACT_QUERY_KEYS.HOMEPAGE_GET_USER_PROVINCES, async () => {
-    const res = await getUserProvince(LIMIT);
-    return res?.items || [];
-  });
-  const { data: userRecentlyLoginData } = useQuery(REACT_QUERY_KEYS.HOMEPAGE_GET_USER_RECENT_LOGIN, async () => {
-    const res = await getUserRecentlyLogin(LIMIT);
-    return res?.items || [];
-  });
-  const { data: userNewMember } = useQuery(REACT_QUERY_KEYS.HOMEPAGE_GET_USER_NEW_MEMBERS, async () => {
-    const res = await getUserNewMembers(LIMIT);
-    return res?.items || [];
-  });
-  const { data: userFavoriteTagsData } = useQuery(REACT_QUERY_KEYS.HOMEPAGE_GET_USER_FAVORITE_TAGS, async () => {
-    const res = await getUserFavoriteTags(LIMIT);
-    return res?.items || [];
-  });
+  // const { data: userProvinceData } = useQuery(REACT_QUERY_KEYS.HOMEPAGE_GET_USER_PROVINCES, async () => {
+  //   const res = await getUserProvince(LIMIT);
+  //   return res?.items || [];
+  // });
+  // const { data: userRecentlyLoginData } = useQuery(REACT_QUERY_KEYS.HOMEPAGE_GET_USER_RECENT_LOGIN, async () => {
+  //   const res = await getUserRecentlyLogin(LIMIT);
+  //   return res?.items || [];
+  // });
+  // const { data: userNewMember } = useQuery(REACT_QUERY_KEYS.HOMEPAGE_GET_USER_NEW_MEMBERS, async () => {
+  //   const res = await getUserNewMembers(LIMIT);
+  //   return res?.items || [];
+  // });
+  // const { data: userFavoriteTagsData } = useQuery(REACT_QUERY_KEYS.HOMEPAGE_GET_USER_FAVORITE_TAGS, async () => {
+  //   const res = await getUserFavoriteTags(LIMIT);
+  //   return res?.items || [];
+  // });
 
   useEffect(() => {
-    setMemberRecommends([
-      // Newest
-      {
-        title: t("home:register-newest"),
-        data: userNewMember?.reverse(),
-      },
+    const fetchData = async () => {
+      const fetchDataPromise = [
+        getUserProvince(LIMIT),
+        getUserRecentlyLogin(LIMIT),
+        getUserNewMembers(LIMIT),
+        getUserFavoriteTags(LIMIT),
+      ];
+      const results = await Promise.all(fetchDataPromise);
+      setMemberRecommends([
+        // Newest
+        {
+          title: t("home:register-newest"),
+          data: results[2]?.items?.reverse() || [],
+        },
 
-      // recent-login-member
-      {
-        title: t("home:recent-login-member"),
-        data: userRecentlyLoginData?.reverse(),
-      },
+        // recent-login-member
+        {
+          title: t("home:recent-login-member"),
+          data: results[1]?.items?.reverse() || [],
+        },
 
-      // member-favorite-area
-      {
-        title: t("home:member-favorite-area"),
-        data: userProvinceData,
-      },
+        // member-favorite-area
+        {
+          title: t("home:member-favorite-area"),
+          data: results[0]?.items || [],
+        },
 
-      // member-favorite-tags
-      {
-        title: t("home:member-favorite-tags"),
-        data: userFavoriteTagsData,
-      },
-    ]);
-  }, [userFavoriteTagsData, userProvinceData, userRecentlyLoginData, userNewMember]);
+        // member-favorite-tags
+        {
+          title: t("home:member-favorite-tags"),
+          data: results[3]?.items || [],
+        },
+      ]);
+    };
+
+    fetchData();
+  }, []);
 
   const [openModal, setOpenModal] = useState(false);
   const [userRequestMatching, setUserRequestMatching] = useState(null);
