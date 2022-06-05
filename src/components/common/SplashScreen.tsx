106c2c96fea57c564b966fd72d1d347776489497
quynh-nguyen <nguyenp_quynh@kiaisoft.com>
2022-06-05 22:31:32 +0700

    add-spash-screen-login

---
+++ src/components/common/SplashScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const SplashScreen = () => (
+  <img
+    alt="splash"
+    src="/assets/images/bg_loading.gif"
+    style={{ top: "40vh", bottom: 0, right: 0, left: "40%", width: "20%", position: "fixed" }}
+  />
+);
+export default SplashScreen;
