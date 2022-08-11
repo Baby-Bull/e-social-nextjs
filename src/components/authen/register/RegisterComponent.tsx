585004fd753fa2280f0d74f3b71b87038b48b95c
Thang Ngo <thangngo24102000tb@gmail.com>
2022-08-11 14:09:15 +0700

    integrate notification system and list notifications

--- src/components/authen/register/RegisterComponent.tsx
+++ src/components/authen/register/RegisterComponent.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 import React, { useRef, useState, useEffect } from "react";
-import { Box, Grid, Typography, Link } from "@mui/material";
+import { Box, Grid, Typography, Link, AppBar, Toolbar } from "@mui/material";
 import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
 import { LoginSocialGoogle, IResolveParams, TypeCrossFunction } from "reactjs-social-login";
@@ -59,7 +59,43 @@ const RegisterComponents = () => {
   return isLoading ? (
     <SplashScreen />
   ) : (
-    <ContentComponent authPage>
+    <ContentComponent authPage showHeader={false}>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar
+          position="fixed"
+          sx={{
+            background: "#fff",
+            boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.1)",
+            p: { xs: 0, lg: "0 16px" },
+          }}
+        >
+          <Toolbar
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              width: { xs: "100%", xl: "1440px" },
+              margin: "auto",
+            }}
+          >
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <Link href="/">
+                <a>
+                  <Box
+                    component="img"
+                    sx={{
+                      width: { xs: "70px", lg: "141px" },
+                      height: { xs: "20px", lg: "42px" },
+                    }}
+                    alt="avatar"
+                    src="/assets/images/logo/logo.png"
+                  />
+                </a>
+              </Link>
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
+
       <Box sx={{ marginTop: "55px" }}>
         <Grid container>
           <GridLeftComponent />
