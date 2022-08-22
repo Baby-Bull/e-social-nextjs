29b857d82dc65dfd873d486b11f7378d59e212c3
Hieu.Do <hieu.do@kiaisoft.com>
2022-08-22 15:21:33 +0700

    added ogp page for community

--- pages/community/[id]/index.tsx
+++ pages/community/[id]/index.tsx
@@ -1,40 +1,45 @@
 import * as React from "react";
-import type { NextPage } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-// import { parseCookies } from "nookies";
+import Head from "next/head";
 
-// import { IS_PROFILE_EDITED, USER_TOKEN } from "src/helpers/storage";
 import CommunityComponent from "src/components/community/IndexComponent";
+import { getCommunity } from "src/services/community";
 
-const sampleCommunityId = "624cf8551b8a720009e2e1db";
-
-const Community: NextPage = () => <CommunityComponent />;
+const Community = ({ communityInfo, url }) => (
+  <React.Fragment>
+    <Head>
+      <meta property="og:type" content="TEST-COMMUNITY" key="og-type" />
+      <meta property="og:title" content={`THIS IS COMMUNITY: ${communityInfo.name}`} key="og-title" />
+      <meta property="og:description" content={communityInfo.description} key="og-description" />
+      <meta property="og:url" content={url} key="og-url" />
+      <meta property="og:image" content={communityInfo.ogp_image} key="og-img" />
+      <meta property="og:site_name" content="GOODHUB エンジニア向けのソフトウェア" key="og-type" />
+      <meta property="og:image:width" content="600" />
+      <meta property="og:image:height" content="315" />
+      <meta name="twitter:card" content={`THIS IS COMMUNITY: ${communityInfo.name}`} key="twitter-card" />
+      <meta name="twitter:url" content={url} key="twitter-url" />
+      <meta name="twitter:image" content={communityInfo.ogp_image} key="twitter-image" />
+      <meta name="twitter:title" content={`THIS IS COMMUNITY: ${communityInfo.name}`} key="twitter-title" />
+      <meta name="twitter:description" content={communityInfo.description} key="twitter-description" />
+      {/* Inject MUI styles first to match with the prepend: true configuration. */}
+    </Head>
+    <CommunityComponent />
+  </React.Fragment>
+);
 
 export const getServerSideProps = async (ctx) => {
   const { locale } = ctx;
-  // const cookies = parseCookies(ctx);
-  // if (cookies[IS_PROFILE_EDITED] === "false") {
-  //   return {
-  //     redirect: {
-  //       destination: "/register/form",
-  //       permanent: false,
-  //     },
-  //   };
-  // }
-  //
-  // if (!cookies[USER_TOKEN]) {
-  //   return {
-  //     redirect: {
-  //       destination: "/login",
-  //       permanent: false,
-  //     },
-  //   };
-  // }
+  const { id } = ctx.params;
+  const [community, translations] = await Promise.all([
+    getCommunity(id),
+    serverSideTranslations(locale, ["common", "community"]),
+  ]);
 
   return {
     props: {
-      ...(await serverSideTranslations(locale, ["common", "community"])),
-      paths: [{ params: { communityId: sampleCommunityId } }],
+      ...translations,
+      communityInfo: community,
+      url: `${process.env.NEXT_PUBLIC_URL_PROFILE}${ctx.resolvedUrl}`,
       fallback: true, // 上記以外のパスでアクセスした場合は 404 ページにしない
     },
   };
