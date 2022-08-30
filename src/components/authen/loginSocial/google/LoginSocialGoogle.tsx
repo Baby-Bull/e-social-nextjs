9bddb11bf434f27c8e03bcea9c974c6f33b1b82e
Hieu.Do <hieu.do@kiaisoft.com>
2022-08-30 10:08:35 +0700

    added new Google auth flow

--- src/components/authen/loginSocial/google/LoginSocialGoogle.tsx
+++ src/components/authen/loginSocial/google/LoginSocialGoogle.tsx
@@ -1,25 +1,21 @@
-import { CodeResponse, GoogleOAuthProvider, useGoogleLogin } from "@react-oauth/google";
+import { GoogleOAuthProvider, useGoogleLogin } from "@react-oauth/google";
 import React, { FC } from "react";
 
 import { IResolveParams } from "..";
 
 type Props = {
   // eslint-disable-next-line no-unused-vars
-  onSuccess: (response: IResolveParams<{ access_token: CodeResponse }>) => void;
+  onSuccess: (response: IResolveParams<any>) => void;
   // eslint-disable-next-line no-unused-vars
   onError: (err: unknown) => void;
   redirectUrl: string;
 };
 
 const GoogleLogin: FC<Props> = ({ onSuccess, onError, redirectUrl, children }) => {
-  const state = (Math.random() + 1).toString(36).substring(7);
-
   const loginGoogle = useGoogleLogin({
-    onSuccess: (code: CodeResponse) => onSuccess({ provider: "google", data: { access_token: code } }),
+    onSuccess: (credentials: any) => onSuccess({ provider: "google", data: credentials }),
     onError,
-    flow: "auth-code",
     redirect_uri: redirectUrl,
-    state,
   });
 
   return <div onClick={loginGoogle}>{children}</div>;
