ce116870a727680896e05d3a28e0cd0fb2e33611
Hieu.Do <hieu.do@kiaisoft.com>
2022-10-17 21:12:21 +0700

    fixed share button

--- src/services/user.ts
+++ src/services/user.ts
@@ -194,22 +194,22 @@ export const UserSearch = async (
   query +=
     params?.lastLogin === typeTimeLogin.on_day_to_week
       ? `&last_login[]=${dayjs().subtract(1, "weeks").toISOString()}&last_login[]=${dayjs()
-        .subtract(1, "days")
-        .toISOString()}`
+          .subtract(1, "days")
+          .toISOString()}`
       : "";
 
   query +=
     params?.lastLogin === typeTimeLogin.week_to_two_week
       ? `&last_login[]=${dayjs().subtract(2, "weeks").toISOString()}&last_login[]=${dayjs()
-        .subtract(1, "weeks")
-        .toISOString()}`
+          .subtract(1, "weeks")
+          .toISOString()}`
       : "";
 
   query +=
     params?.lastLogin === typeTimeLogin.two_week_to_month
       ? `&last_login[]=${dayjs().subtract(1, "months").toISOString()}&last_login[]=${dayjs()
-        .subtract(2, "weeks")
-        .toISOString()}`
+          .subtract(2, "weeks")
+          .toISOString()}`
       : "";
 
   query +=
